65b2915e57ae77bd40642c5577a85aa815f9021a
Cristian <cristi.valentin29@gmail.com>
2021-09-02 17:40:49 +0200

    feat: 🎸 set up spring environments

--- apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/JavaApiApplication.java
@@ -1,10 +1,22 @@
 package org.scoalaonline.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaApiApplication {
+
+  @Value("${spring.application.name}")
+  String applicationName;
+
+  @GetMapping("/")
+  public String home() {
+    return "Welcome to " + applicationName;
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApiApplication.class, args);
