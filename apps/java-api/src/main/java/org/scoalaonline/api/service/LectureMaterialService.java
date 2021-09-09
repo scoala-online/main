209de41e86cbc142950121fa08f3e340e0ba05a7
Robert Mrn <rmrn56@gmail.com>
2021-09-09 18:34:58 +0300

    feat: 🎸 Add LectureMaterialService service

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/service/LectureMaterialService.java
@@ -0,0 +1,49 @@
+package org.scoalaonline.api.service;
+
+import org.scoalaonline.api.model.LectureMaterial;
+import org.scoalaonline.api.repository.LectureMaterialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LectureMaterialService implements ServiceInterface<LectureMaterial>{
+
+  @Autowired
+  LectureMaterialRepository lectureMaterialRepository;
+
+
+  @Override
+  public List<LectureMaterial> getAll() {
+    return lectureMaterialRepository.findAll();
+  }
+
+  @Override
+  public Optional<LectureMaterial> getOneById(Long id) {
+    return lectureMaterialRepository.findById(id);
+  }
+
+  @Override
+  public LectureMaterial add(LectureMaterial entry) {
+    LectureMaterial lectureMaterialToSave = new LectureMaterial();
+    lectureMaterialToSave.setDocument(entry.getDocument());
+
+    return lectureMaterialRepository.save(lectureMaterialToSave);
+  }
+
+  @Override
+  public LectureMaterial update(Long id, LectureMaterial object) {
+    LectureMaterial lectureMaterialToUpdate = lectureMaterialRepository.findById(id).get();
+    if(object.getDocument() != null) {
+      object.setDocument(object.getDocument());
+    }
+    return lectureMaterialRepository.save(lectureMaterialToUpdate);
+  }
+
+  @Override
+  public void delete(Long id) {
+    lectureMaterialRepository.deleteById(id);
+  }
+}
