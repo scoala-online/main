3e76f45aae45130c81a8d6f8c2a6137b2c52917c
Robert Mrn <rmrn56@gmail.com>
2021-09-10 18:18:34 +0300

    feat: 🎸 Add documentation comments to controller

--- apps/java-api/src/main/java/org/scoalaonline/api/controller/LectureMaterialController.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/controller/LectureMaterialController.java
@@ -1,5 +1,6 @@
 package org.scoalaonline.api.controller;
 
+import org.scoalaonline.api.exception.LectureMaterialInvalidDataException;
 import org.scoalaonline.api.exception.LectureMaterialNotFoundException;
 import org.scoalaonline.api.model.LectureMaterial;
 import org.scoalaonline.api.service.LectureMaterialService;
@@ -19,34 +20,71 @@ public class LectureMaterialController {
   @Autowired
   LectureMaterialService lectureMaterialService;
 
+
+  /**
+   * Sends an HTTP Response Entity with all the history entries
+   * @return a Response Entity
+   *         with HTTP Status OK and a list of the history entries
+   */
   @GetMapping(value = {"", "/"})
   public ResponseEntity<List<LectureMaterial>> getAllLectureMaterials () {
     List<LectureMaterial> lectureMaterials = lectureMaterialService.getAll();
     return new ResponseEntity<>(lectureMaterials, HttpStatus.OK);
   }
 
+  /**
+   * Sends an HTTP Response Entity with a specific history entry and
+   * Status OK or HTTP Status Not Found if there is no entry with the provided id
+   * @param id
+   * @return the Response Entity with a Status Code and a Body
+   */
   @GetMapping(value = "/{id}")
-  public ResponseEntity<LectureMaterial> getArtistById(@PathVariable("id") String id) {
-    LectureMaterial artist = lectureMaterialService.getOneById(id)
-      .orElseThrow(() -> new ResponseStatusException(
-        HttpStatus.NOT_FOUND, "GET: Lecture Material not found.", new LectureMaterialNotFoundException()
-      ));
-    return new ResponseEntity<>(artist, HttpStatus.OK);
+  public ResponseEntity<LectureMaterial> getLectureMaterialById(@PathVariable("id") String id) {
+    LectureMaterial lectureMaterial;
+    try
+    {
+      lectureMaterial = lectureMaterialService.getOneById(id).orElseThrow(
+        () -> new LectureMaterialNotFoundException("Method getOneById: Lecture Material not found")
+      );
+    } catch (LectureMaterialNotFoundException e)
+    {
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "GET: Lecture Material Not Found", e );
+    }
+    return new ResponseEntity<>(lectureMaterial, HttpStatus.OK);
   }
 
+  /**
+   * Sends an HTTP Response Entity with the history entry that has been
+   * created, along with Status Created
+   * @param lectureMaterial
+   * @return the Response Entity with a Status Code and a Body
+   */
   @PostMapping(value = {"", "/"})
-  public ResponseEntity<LectureMaterial> addArtist (@RequestBody LectureMaterial lectureMaterial) {
-    LectureMaterial savedArtist = lectureMaterialService.add(lectureMaterial);
-    return new ResponseEntity<>(savedArtist, HttpStatus.CREATED);
+  public ResponseEntity<LectureMaterial> addLectureMaterial (@RequestBody LectureMaterial lectureMaterial) {
+    LectureMaterial savedLectureMaterial;
+    try {
+      savedLectureMaterial = lectureMaterialService.add(lectureMaterial);
+    } catch (LectureMaterialInvalidDataException e) {
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "POST: Lecture Material Invalid Data", e );
+    }
+    return new ResponseEntity<>(savedLectureMaterial, HttpStatus.CREATED);
   }
 
+  /**
+   * Sends an HTTP Response Entity with the history entry that has been
+   * updated, along with the Status OK, or Status Not Found if there
+   * is no entry with the provided id
+   * @param id
+   * @param lectureMaterial
+   * @return the Response Entity with a Status Code and a Body
+   */
   @PutMapping( value = "/{id}" )
-  public ResponseEntity<LectureMaterial> update( @PathVariable( "id" ) String id, @RequestBody LectureMaterial lectureMaterial ) {
+  public ResponseEntity<LectureMaterial> updateLectureMaterial( @PathVariable( "id" ) String id, @RequestBody LectureMaterial lectureMaterial ) {
     LectureMaterial updatedLectureMaterial;
     try
     {
       updatedLectureMaterial = lectureMaterialService.update( id, lectureMaterial );
-    } catch ( LectureMaterialNotFoundException e )
+    } catch ( LectureMaterialNotFoundException | LectureMaterialInvalidDataException e )
     {
       throw new ResponseStatusException( HttpStatus.NOT_FOUND, "PUT: Lecture Material Not Found", e );
     }
@@ -54,16 +92,21 @@ public class LectureMaterialController {
     return new ResponseEntity<>( updatedLectureMaterial, HttpStatus.OK );
   }
 
+  /**
+   * Deletes a history entry and sends an HTTP Response Entity
+   * with the Status OK or Not Found if there is no entry with the provided id
+   * @param id
+   * @return a Response Entity with Status OK
+   */
   @DeleteMapping( value = "/{id}" )
-  public ResponseEntity<HttpStatus> delete( @PathVariable( "id" ) String id ) {
+  public ResponseEntity<HttpStatus> deleteLectureMaterial( @PathVariable( "id" ) String id ) {
     try
     {
       lectureMaterialService.delete( id );
     } catch ( LectureMaterialNotFoundException e )
     {
-      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "DELETE: History Not Found", e );
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "DELETE: Lecture Material Not Found", e );
     }
-
     return new ResponseEntity<>( HttpStatus.OK );
   }
 
