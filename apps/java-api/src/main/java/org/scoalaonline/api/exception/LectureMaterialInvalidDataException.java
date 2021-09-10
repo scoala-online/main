bdd7291ad9971201d82adcd9d5c81858e69d9ad5
Robert Mrn <rmrn56@gmail.com>
2021-09-10 18:18:11 +0300

    feat: 🎸 Add new Exception for LectrueMaterial model

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/exception/LectureMaterialInvalidDataException.java
@@ -0,0 +1,10 @@
+package org.scoalaonline.api.exception;
+
+public class LectureMaterialInvalidDataException extends Exception{
+  public LectureMaterialInvalidDataException(){
+
+  }
+  public LectureMaterialInvalidDataException(String message){
+    super(message);
+  }
+}
