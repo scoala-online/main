071779b208290cf7d5f0d1f8eb49abbb5e4c973a
Robert Mrn <rmrn56@gmail.com>
2021-09-16 12:55:41 +0300

    test: 💍 Add test for getOneById() method

--- apps/java-api/src/test/java/org/scoalaonline/api/service/LectureMaterialServiceTest.java
+++ apps/java-api/src/test/java/org/scoalaonline/api/service/LectureMaterialServiceTest.java
@@ -1,33 +1,38 @@
 package org.scoalaonline.api.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.scoalaonline.api.exception.LectureMaterialNotFoundException;
+import org.scoalaonline.api.model.LectureMaterial;
 import org.scoalaonline.api.repository.LectureMaterialRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class LectureMaterialServiceTest {
 
   @Mock
   private LectureMaterialRepository lectureMaterialRepository;
-  private LectureMaterialService underTest;
+  private LectureMaterialService underTestService;
 
   @BeforeEach
   void setUp() {
-    underTest = new LectureMaterialService(lectureMaterialRepository);
+    underTestService = new LectureMaterialService(lectureMaterialRepository);
   }
 
 
   @Test
   void getAllTest() {
     // when
-    underTest.getAll();
+    underTestService.getAll();
 
     // then
     verify(lectureMaterialRepository).findAll();
@@ -35,7 +40,15 @@ class LectureMaterialServiceTest {
 
   @Test
   @Disabled
-  void getOneById() {
+  void getOneByIdTest() throws LectureMaterialNotFoundException {
+    // when
+    when(lectureMaterialRepository.findById(anyString()))
+      .thenReturn(java.util.Optional.of(new LectureMaterial("id", "Document.pdf")));
+
+    // then
+    LectureMaterial lectureMaterial = underTestService.getOneById("id");
+
+    Assertions.assertEquals("Document.pdf", lectureMaterial.getDocument());
   }
 
   @Test
