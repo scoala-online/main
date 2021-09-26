e610e264014ae2a1eb0f6777ca182e0dd894e403
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-09-26 17:07:13 +0300

    feat: 🎸 Add routes
    Add routes

--- apps/e-learning-app/src/app/app.js
+++ apps/e-learning-app/src/app/app.js
@@ -1,7 +1,7 @@
 import styles from './app.module.css';
 import { ReactComponent as Logo } from './logo.svg';
 
-import { Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { environment } from '../environments/environment';
 
 import HomePage from './components/homePage.component'
@@ -9,55 +9,11 @@ import HomePage from './components/homePage.component'
 export function App() {
   return (
     <div className={styles.app}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to e-learning-app!</h1>
-      </header>
-      <main>
-        <p>Current Environment: {environment.envName}</p>
-        <p>Current Environment from .env: {process.env.NX_ENVIRONMENT}</p>
-      </main>
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/test"
-        exact>
-          <HomePage />
-      </Route>
-      {/* END: routes */}
+      <Router>
+        <Switch>
+          <Route exact path={'/'} component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- apps/e-learning-app/src/app/components/homePage.component.js
+++ apps/e-learning-app/src/app/components/homePage.component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class HomePage extends React.Component {
 
