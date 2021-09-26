841413c0f9fcc0ac15ae8731e3b7d75d59a8af35
Robert Mrn <rmrn56@gmail.com>
2021-09-26 19:08:51 +0300

    feat: 🎸 Add Grade controller

---
+++ apps/java-api/src/main/java/org/scoalaonline/api/controller/GradeController.java
@@ -0,0 +1,117 @@
+package org.scoalaonline.api.controller;
+
+import org.scoalaonline.api.exception.grade.GradeInvalidValueException;
+import org.scoalaonline.api.exception.grade.GradeNotFoundException;
+import org.scoalaonline.api.model.Grade;
+import org.scoalaonline.api.service.GradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+/**
+ GET:	"/grade"	retrieves all the entries
+ GET:	"/grade/{id}"	retrieves the entry with the provided id
+ POST: "/grade"	creates a new entry
+ PATCH:	"/grade/{id}	edits the entry with the provided id
+ DELETE:	"/grade/{id}	deletes the entry with the provided id
+ */
+@CrossOrigin
+@RestController
+@RequestMapping("/grade")
+public class GradeController {
+  @Autowired
+  GradeService gradeService;
+
+  /**
+   * Sends an HTTP Response Entity with all the grade entries
+   * @return a Response Entity
+   *         with HTTP Status OK and a list of the grade entries
+   */
+  @GetMapping(value = {"", "/"})
+  public ResponseEntity<List<Grade>> getAllGrades () {
+    List<Grade> grades = gradeService.getAll();
+    return new ResponseEntity<>(grades, HttpStatus.OK);
+  }
+
+  /**
+   * Sends an HTTP Response Entity with a specific grade entry and
+   * Status OK or HTTP Status Not Found if there is no entry with the provided id
+   * @param id
+   * @return the Response Entity with a Status Code and a Body
+   */
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<Grade> getGradeById(@PathVariable("id") String id) {
+    Grade grade;
+    try
+    {
+      grade = gradeService.getOneById(id);
+    } catch (GradeNotFoundException e)
+    {
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "GET: Grade Not Found", e );
+    }
+    return new ResponseEntity<>(grade, HttpStatus.OK);
+  }
+
+  /**
+   * Sends HTTP status Response Entity with the Grade entry that has been created.
+   * Sends HTTP status Invalid Value if the Grade to be posted is invalid.
+   * @param grade the Grade to be added in the db.
+   * @return the Response Entity with a Status Code and a body.
+   */
+  @PostMapping(value = {"", "/"})
+  public ResponseEntity<Grade> addGrade (@RequestBody Grade grade) {
+    Grade savedGrade;
+    try {
+      savedGrade = gradeService.add(grade);
+    } catch (GradeInvalidValueException e) {
+      throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "POST: Grade Invalid Document", e );
+    }
+    return new ResponseEntity<>(savedGrade, HttpStatus.CREATED);
+  }
+
+  /**
+   * Sends HTTP status Response Entity with the Grade entry that has been updated.
+   * Sends HTTP status Not Found if the Grade cannot be found.
+   * Sends HTTP status Invalid Value if the Grade to be posted is invalid.
+   * @param id the id of the Grade to be updated.
+   * @param grade the Grade to be updated.
+   * @return the Response Entity with a Status Code with a body.
+   */
+  @PatchMapping( value = "/{id}" )
+  public ResponseEntity<Grade> updateGrade (@PathVariable( "id" ) String id, @RequestBody Grade grade ) {
+    Grade updatedGrade;
+    try
+    {
+      updatedGrade = gradeService.update( id, grade );
+    } catch ( GradeNotFoundException e )
+    {
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "PATCH: Grade Not Found", e );
+    } catch ( GradeInvalidValueException e){
+      throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "PATCH: Grade Invalid Document", e );
+    }
+
+    return new ResponseEntity<>( updatedGrade, HttpStatus.OK );
+  }
+
+  /**
+   * Deletes a Grade entry and sends an HTTP Response Entity
+   * with the Status OK or Not Found if there is no entry with the provided id
+   * @param id
+   * @return a Response Entity with Status OK
+   */
+  @DeleteMapping( value = "/{id}" )
+  public ResponseEntity<HttpStatus> deleteGrade( @PathVariable( "id" ) String id ) {
+    try
+    {
+      gradeService.delete( id );
+    } catch ( GradeNotFoundException e )
+    {
+      throw new ResponseStatusException( HttpStatus.NOT_FOUND, "DELETE: Grade Not Found", e );
+    }
+    return new ResponseEntity<>( HttpStatus.OK );
+  }
+}
