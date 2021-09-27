78259565520bb6c84247b878d22bce7f018c6d43
rmrn65 <45951811+rmrn65@users.noreply.github.com>
2021-09-27 22:05:40 +0300

    Update apps/java-api/src/main/java/org/scoalaonline/api/controller/GradeController.java
    Co-authored-by: CristiPV <cristi.valentin29@gmail.com>

--- apps/java-api/src/main/java/org/scoalaonline/api/controller/GradeController.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/controller/GradeController.java
@@ -77,7 +77,7 @@ public class GradeController {
    * Sends HTTP Response Entity with the Grade entry that has been updated.
    * Sends HTTP status Not Found if the Grade cannot be found.
    * Sends HTTP status Bad Request if the Grade to be posted is invalid.
-   * @param id the id of the Grade to be updated.
+   * @param id - the id of the Grade to be updated.
    * @param grade the Grade to be updated.
    * @return the Response Entity with a Status Code with a body.
    */
