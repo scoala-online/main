dd63d39e23f622b06905ee872fccbcdf8247edb1
Alexandru Istrate <38143225+AlexandruIstrate@users.noreply.github.com>
2021-10-03 00:37:44 +0300

    feat: 🎸 Add item sizes

--- apps/e-learning-app/src/app/components/homePage.component.js
+++ apps/e-learning-app/src/app/components/homePage.component.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+
+import Card from 'react-bootstrap/Card'
 import Carousel from 'react-bootstrap/Carousel'
 
 import { withRouter } from 'react-router-dom';
@@ -10,36 +13,156 @@ class HomePage extends Component {
 
   render() {
     const mainContanierStyle = {
-      height: '5px'
+      background: '#dddddd'
+    }
+
+    const titleStyle = {
+      marginTop: '26vh',
+      marginBottom: '39vh',
+      marginLeft: '13vw',
+      marginRight: '13vw'
+    }
+
+    const projectInfoStyle = {
+      marginTop: '10vh',
+      marginBottom: '19vh',
+      marginLeft: '4vw',
+      marginRight: '4vw'
+    }
+
+    const cardStyle = {
+      background: '#cccccc',
+      width: '18vw',
+      height: '40vh'
+    }
+
+    const carouselContainerStyle = {
+      marginTop: '14vh',
+      marginBottom: '17vh',
+      marginLeft: '7vw',
+      marginRight: '7vw'
+    }
+
+    const carouselStyle = {
+      width: '52vw',
+      height: '51vh'
+    }
+
+    const carouselCardStyle = {
+      background: '#446677',
+      width: '52vw',
+      height: '51vh'
     }
 
     return (
-      <Container style={mainContanierStyle}>
+      <Container fluid style={mainContanierStyle}>
         <Row>
-          <Col />
+          <Container fluid style={titleStyle}>
+            <Row>
+              <h1>Scoala Online</h1>
+            </Row>
+            <Row>
+              <h2>12 ani de educatie. Un singur site.</h2>
+            </Row>
+            <Row>
+              <h3>
+                Invata oriunde vrei, in ritmul tau. <br />
+                Alege profesorul in functie de ce stil de explicatie preferi.
+              </h3>
+            </Row>
+          </Container>
+        </Row>
+        <Row>
           <Col>
-            <Container>
+            <Container fluid style={projectInfoStyle}>
               <Col>
-                <Row>Header and Title</Row>
                 <Row>
-                  <h2>Ce ne dorim</h2>
-                  <Container>
-                    <Row>
-                      <Col>1</Col>
-                      <Col>2</Col>
-                      <Col>3</Col>
-                    </Row>
-                  </Container>
+                  <div className='p-5 text-center'>
+                    <h2 className='mb-3'>Ce ne dorim sa obtinem din acest proiect?</h2>
+                  </div>
                 </Row>
                 <Row>
-                  <Carousel>
-
-                  </Carousel>
+                  <Container fluid>
+                    <Row>
+                      <Col>
+                        <Card className="text-center" style={cardStyle}>
+                          <Card.Body>
+                            <Card.Title>Invatamant gratuit</Card.Title>
+                            <Card.Text>
+                              Intreaga materie scolara a fost filmata si oricine are acces nelimitat si gratuit la continut
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                      <Col>
+                        <Card className="text-center" style={cardStyle}>
+                          <Card.Body>
+                            <Card.Title>Invatamant gratuit</Card.Title>
+                            <Card.Text>
+                              Intreaga materie scolara a fost filmata si oricine are acces nelimitat si gratuit la continut
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                      <Col>
+                        <Card className="text-center" style={cardStyle}>
+                          <Card.Body>
+                            <Card.Title>Invatamant gratuit</Card.Title>
+                            <Card.Text>
+                              Intreaga materie scolara a fost filmata si oricine are acces nelimitat si gratuit la continut
+                            </Card.Text>
+                          </Card.Body>
+                        </Card>
+                      </Col>
+                    </Row>
+                  </Container>
                 </Row>
               </Col>
             </Container>
           </Col>
-          <Col />
+        </Row>
+        <Row>
+          <Container fluid style={carouselContainerStyle}>
+            <Row>
+              <Carousel style={carouselStyle}>
+                <Carousel.Item>
+                  <div style={carouselCardStyle}>
+                    <h1>
+                      Hello 1
+                    </h1>
+                  </div>
+                  <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <div style={carouselCardStyle}>
+                    <h1>
+                      Hello 2
+                    </h1>
+                  </div>
+
+                  <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <div style={carouselCardStyle}>
+                    <h1>
+                      Hello 3
+                    </h1>
+                  </div>
+
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </Row>
+          </Container>
         </Row>
       </Container>
     );
