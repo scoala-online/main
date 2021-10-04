a611cb711544d8272e07b55a216dfa1c090e3dd3
Robert Mrn <rmrn56@gmail.com>
2021-10-04 21:19:32 +0300

    feat: 🎸 Add Sidebar static data

---
+++ apps/e-learning-app/src/components/SidebarData.js
@@ -0,0 +1,23 @@
+export const SidebarData = [
+  {
+    subtitle: 'Fronda in literatura interbelica',
+    path: '',
+  },
+  {
+    subtitle: 'Ion Vinea: \'Manifest activist catre tinerime\'',
+    path: '',
+  },
+  {
+    subtitle: 'Tristan Tzara: \'Manifest despre amorul slab si amorul amar\'',
+    path: '',
+  },
+  {
+    title:'Limba si comunicare',
+    subtitles:[
+      {
+        subtitle:'Punctuatia si justificarile ei sintactice si stilistice',
+        path: '',
+      },
+    ]
+  }
+];
