9b7f324654caf34da7a66af895cdd469de2ae28f
Robert Mrn <rmrn56@gmail.com>
2021-10-13 16:05:15 +0300

    feat: 🎸 Connect test file to database

--- apps/java-api/src/test/java/org/scoalaonline/api/integration/LectureMaterialControllerIntegrationTest.java
+++ apps/java-api/src/test/java/org/scoalaonline/api/integration/LectureMaterialControllerIntegrationTest.java
@@ -1,11 +1,15 @@
 package org.scoalaonline.api.integration;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.scoalaonline.api.controller.LectureMaterialController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockServletContext;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -15,10 +19,17 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.ServletContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 @ExtendWith(SpringExtension.class)
-//@ContextConfiguration(classes = { ApplicationConfig.class })
+@SpringBootTest
+@WithMockUser(roles={"ADMIN"})
 @WebAppConfiguration
 public class LectureMaterialControllerIntegrationTest {
+
   @Autowired
   private WebApplicationContext webApplicationContext;
 
@@ -32,10 +43,13 @@ public class LectureMaterialControllerIntegrationTest {
   public void givenWac_whenServletContext_thenItProvidesGreetController() {
     ServletContext servletContext = webApplicationContext.getServletContext();
 
-    Assert.assertNotNull(servletContext);
-    Assert.assertTrue(servletContext instanceof MockServletContext);
-    Assert.assertNotNull(webApplicationContext.getBean("greetController"));
+    Assertions.assertNotNull(servletContext);
+    Assertions.assertTrue(servletContext instanceof MockServletContext);
   }
 
-  
+  @Test
+  public void getAllTest() throws Exception {
+    this.mockMvc.perform(get("/lecture-materials")).andDo(print())
+      .andExpect(status().isOk());
+  }
 }
