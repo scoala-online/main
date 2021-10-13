ced46d7bc4dac053746f008304f7b7b675ed7fe9
Marian <wyz_marian_013@yahoo.com>
2021-10-13 18:11:29 +0300

    feat: 🎸 Change app component

--- apps/e-learning-app/src/app/app.js
+++ apps/e-learning-app/src/app/app.js
@@ -1,57 +1,20 @@
-import styles from './app.module.css';
-import { ReactComponent as Logo } from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Route, Link } from 'react-router-dom';
-import { environment } from '../environments/environment';
+import Layout from './components/Layout';
+
+import './app.module.css';
+
 
 export function App() {
   return (
-    <div className={styles.app}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to e-learning-app!</h1>
-      </header>
-      <main>
-        <p>Current Environment: {environment.envName}</p>
-        <p>Current Environment from .env: {process.env.NX_ENVIRONMENT}</p>
-      </main>
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Route
-        path="/"
-        exact
-        render={() => (
-          <div>
-            This is the generated root route.{' '}
-            <Link to="/page-2">Click here for page 2.</Link>
-          </div>
-        )}
-      />
-      <Route
-        path="/page-2"
-        exact
-        render={() => (
-          <div>
-            <Link to="/">Click here to go back to root page.</Link>
-          </div>
-        )}
-      />
-      {/* END: routes */}
-    </div>
+    <div className="App">
+    <Router>
+      <Switch>
+        <Layout>
+        </Layout>
+      </Switch>
+    </Router>
+  </div>
   );
 }
 export default App;
