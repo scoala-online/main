e99fa83043ff59bc6428bcfcdc8ff81ec01ceac5
Robert Mrn <rmrn56@gmail.com>
2021-10-14 21:46:44 +0300

    test: 💍 Add test for add exception "invalid data"

--- apps/java-api/src/test/java/org/scoalaonline/api/integration/LectureMaterialControllerIntegrationTest.java
+++ apps/java-api/src/test/java/org/scoalaonline/api/integration/LectureMaterialControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package org.scoalaonline.api.integration;
 
 import net.minidev.json.parser.JSONParser;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
@@ -13,7 +14,9 @@ import org.scoalaonline.api.model.LectureMaterial;
 import org.scoalaonline.api.repository.LectureMaterialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
@@ -32,12 +35,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.scoalaonline.api.util.TestUtils.buildJsonBody;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -83,7 +86,7 @@ public class LectureMaterialControllerIntegrationTest {
     Optional<LectureMaterial> entity = lectureMaterialRepository.findById(lectureMaterialToSave.getId());
     assertThat(entity.get().getDocument()).isEqualTo("EXAMPLE_DOCUMENT.txt");
   }
-
+  
   @Test
   public void addLectureMaterialTest() throws Exception {
     List<String> FieldArray = new ArrayList<String>();
@@ -92,18 +95,51 @@ public class LectureMaterialControllerIntegrationTest {
     ValuesArray.add("EXAMPLE_POST_DOCUMENT.pdf");
     StringWriter jsonObjectWriter = buildJsonBody(FieldArray, ValuesArray);
 
-    MvcResult obj = this.mockMvc.perform(post("/lecture-materials")
+    MockHttpServletResponse response = this.mockMvc.perform(post("/lecture-materials")
       .contentType(MediaType.APPLICATION_JSON)
       .content(jsonObjectWriter.toString())).andDo(print())
-      .andExpect(status().isCreated()).andReturn();
+      .andExpect(status().isCreated()).andReturn().getResponse();
 
-    String s = obj.getResponse().getContentAsString();
-    JSONObject parsedLectureMaterial = new JSONObject(s) ;
+    String responseToString = response.getContentAsString();
+    JSONObject parsedLectureMaterial = new JSONObject(responseToString) ;
 
     Optional<LectureMaterial> entity = lectureMaterialRepository.findById(parsedLectureMaterial.get("id").toString());
     assertThat(entity.get()).isNotNull();
     assertThat(entity.get().getDocument()).isEqualTo("EXAMPLE_POST_DOCUMENT.pdf");
     lectureMaterialRepository.delete(entity.get());
   }
-  
+
+  @Test
+  void addLectureMaterialInvalidDataExceptionTest() throws Exception {
+
+    List<Object> exceptionCases = new ArrayList<Object>();
+    exceptionCases.add("");
+    exceptionCases.add(null);
+    for(Object exceptionCase : exceptionCases) {
+
+      //Json Generator
+      List<String> FieldArray = new ArrayList<String>();
+      FieldArray.add("document");
+      List<Object> ValuesArray = new ArrayList<Object>();
+      ValuesArray.add(exceptionCase);
+      StringWriter jsonObjectWriter = buildJsonBody(FieldArray, ValuesArray);
+
+      //when
+      MockHttpServletResponse response = mockMvc.perform(
+        post("/lecture-materials").contentType(MediaType.APPLICATION_JSON)
+          .content(jsonObjectWriter.toString()))
+        .andReturn().getResponse();
+
+      //in case it fails, delete the unwanted entry
+      if(!response.getContentAsString().isEmpty()) {
+        JSONObject parsedLectureMaterial = new JSONObject(response.getContentAsString());
+        lectureMaterialRepository.deleteById(parsedLectureMaterial.get("id").toString());
+      }
+
+      //then
+      AssertionsForClassTypes.assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+      AssertionsForClassTypes.assertThat(response.getContentAsString()).isEmpty();
+      assertThat(response.getErrorMessage()).isEqualTo("POST: Lecture Material Invalid Document");
+    }
+  }
 }
