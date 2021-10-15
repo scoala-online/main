6cb10d64e3492417c58c0c20d6c4cf6cfc92ef17
Cristian <cristi.valentin29@gmail.com>
2021-10-15 12:31:15 +0200

    docs: ✏️ add todos

---
+++ apps/todos.js
@@ -0,0 +1,31 @@
+// TODO: change assets/images/Modules to assets/images/tmp - to indicate that those images are only used 
+// temporarily, and that they need to be deleted once the connection to the database is established.
+
+// TODO: remove logo.svg and star.svg as they are no longer needed
+
+// TODO: fix HUGE fonts
+
+// TODO: fix scaling on smaller devices
+
+// TODO: fix sidebar not extending to the bottom of the page
+
+// TODO: add a folder for each component and separate the style from functionality
+
+// TODO: separate pages from components
+
+// TODO: Make components a bit more modular - videoContent, sidebar
+
+// TODO: resolve warnings - sidebar, video
+
+// TODO: add comments
+
+// TODO: refactor for react bootstrap
+
+// TODO: rename some files - VideoContent -> ModuleContent; VideosRow -> ContentGrid/List, 
+// SidebarData -> MockData ( and put everything in it )
+
+// TODO: make the videosRow a responsive grid -> https://react-bootstrap.github.io/layout/grid/
+
+// TODO: separate mocked data from real data, like in sidebarData
+
+// TODO: delete this file once all changes are applied
