df318cea9779076829d02d25811a9ad5268d9aea
Marian <wyz_marian_013@yahoo.com>
2021-10-22 13:58:00 +0300

    feat: 🎸 Testing

--- apps/e-learning-app/src/app/todos.js
+++ apps/e-learning-app/src/app/todos.js
@@ -1,11 +1,11 @@
-// TODO: Fix logo scaling ( should scale with vmin )
+// TODO: Fix logo scaling ( should scale with vmin ) x
 
-// TODO: Fix navbar scaling - add minimum height
+// TODO: Fix navbar scaling - add minimum height x
 
-// TODO: Make All grades ( 0 - 13 ) fit on the page
+// TODO: Make All grades ( 0 - 13 ) fit on the page x
 
-// TODO: Rename posSubject to subjectPos in Sidebar
+// TODO: Rename posSubject to subjectPos in Sidebar x
 
-// TODO: add comments on components describing what they render, what their state, and props are
+// TODO: add comments on components describing what they render, what their state, and props are x
 
-// TODO: add comments on functions and useEffects to explain what they do, javadoc style
+// TODO: add comments on functions and useEffects to explain what they do, javadoc style x
