981fe7bd81823bf7cd4685c79f515c50cc59748b
Marian <wyz_marian_013@yahoo.com>
2021-10-22 14:06:09 +0300

    feat: 🎸 Add dimensions state

--- apps/e-learning-app/src/app/app.js
+++ apps/e-learning-app/src/app/app.js
@@ -1,12 +1,44 @@
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
+import Test from './components/Test';
 
+/**
+ * App renders the Layout component.
+ * State:
+ * - dimensions: object ( contains the current height and width of the viewport )
+ */
 export function App() {
+  // State
+  const [dimensions, setDimensions] = useState({ 
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
+
+  /**
+   * Changes the dimensions state whenever the viewport sizes are changed.
+   */
+  useEffect(() => {
+    function handleResize() {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return _ => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
   return (
     <Router>
       <Switch>
-        <Layout>
+        <Layout dimensions={dimensions}>
+        <Route path="/" component={Test} />
         </Layout>
       </Switch>
     </Router>
