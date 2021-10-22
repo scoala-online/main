8dccb07db0ec45ca8484086569b41bf581bacb21
Marian <wyz_marian_013@yahoo.com>
2021-10-22 13:58:50 +0300

    feat: 🎸 Add sizes

--- apps/e-learning-app/src/app/themes/Sizes.js
+++ apps/e-learning-app/src/app/themes/Sizes.js
@@ -1,2 +1,29 @@
-export const sidebarButtonHeight = 6.85;
-export const navBarHeight = 6.85;
+export const navBarHeight = 6.66;
+export const minNavBarHeight = 40;
+
+export const sidebarButtonHeight = 6.66;
+export const minSidebarButtonHeight = 40;
+export const minSidebarButtonWidth = 105;
+export const gradeSidebarButtonWidth = 15.8;
+export const subjectSidebarButtonWidth = 12.2;
+
+export const materialsButtonHeight = 8.8;
+export const minMaterialsButtonHeight = 55;
+export const minMaterialsButtonWidth = 102;
+export const materialsButtonTop = -1.6;
+export const minMaterialsButtonTop = -13;
+
+export const dropdownIconHeight = 3.05;
+export const minDropdownIconHeight = 15;
+export const dropdownIconTop = 3.05;
+export const minDropdownIconTop = 18;
+
+export const logoHeight = 9;
+export const minLogoHeight = 55;
+export const logoTop = -1;
+export const minLogoTop = -8;
+
+export const searchInputFieldHeight = 3.15;
+export const minSearchInputFieldHeight = 18;
+export const searchInputFieldTop = 1.85;
+export const minSearchInputFieldTop = 11;
