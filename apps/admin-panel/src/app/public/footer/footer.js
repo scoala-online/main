f9f0ebf7c0fe067140e452e90f5848ff76d229f2
Cristian <cristi.valentin29@gmail.com>
2021-11-28 10:35:09 +0100

    feat: 🎸 remove old app

--- apps/admin-panel/src/app/public/footer/footer.js
+++
@@ -1,7 +0,0 @@
-const copyrightSpan = document
-  .getElementsByClassName('footer')[0]
-  .getElementsByClassName('right')[0]
-  .getElementsByClassName('copyright')[0];
-const year = new Date().getFullYear();
-
-copyrightSpan.innerText = '@Copyright ' + year;
