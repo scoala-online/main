a21f0d18fbf2bddccae71e95661f04cdbe5546f4
Yan Badea <yan_badea@yahoo.com>
2021-12-02 22:37:34 +0000

    test: 💍 complete testUtils to cover Duration class
    ✅ Closes: #21

--- apps/java-api/src/test/java/org/scoalaonline/api/util/TestUtils.java
+++ apps/java-api/src/test/java/org/scoalaonline/api/util/TestUtils.java
@@ -2,9 +2,13 @@ package org.scoalaonline.api.util;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.time.Duration;
 import java.util.List;
 
 public class TestUtils {
@@ -18,8 +22,9 @@ public class TestUtils {
     JsonGenerator generator = factory.createGenerator(jsonObjectWriter);
 
     generator.useDefaultPrettyPrinter();
-    generator.useDefaultPrettyPrinter();
     generator.writeStartObject();
+    ObjectMapper mapper = JsonMapper.builder().addModule(new JavaTimeModule()).build();
+    generator.setCodec(mapper);
 
     for(int i = 0; i < fields.size(); i++) {
       generator.writeFieldName(fields.get(i));
@@ -29,6 +34,8 @@ public class TestUtils {
         generator.writeString((String) values.get(i));
       else if(values.get(i).getClass().equals(Double.class))
         generator.writeNumber((double) values.get(i));
+      else if(values.get(i).getClass().equals(Duration.class))
+        generator.writeObject((Duration)values.get(i));
       /* For more possible cases, add else if here
        With the required type
        Example:
