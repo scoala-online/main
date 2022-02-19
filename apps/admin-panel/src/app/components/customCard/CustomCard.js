e5b84a8f35a216ae74bd8cd68c7555eef9b6e290
Cristian <cristi.valentin29@gmail.com>
2022-02-19 16:17:02 +0100

    feat: 🎸 add custom card component

---
+++ apps/admin-panel/src/app/components/customCard/CustomCard.js
@@ -0,0 +1,29 @@
+import { Card } from 'react-bootstrap';
+
+/**
+ * Renders a Card with a custom Title, Description, and link button.
+ * @param {*} props
+ * >
+ * - `String` title
+ * - `String` description
+ * - `String` buttonText
+ * - `String` buttonLink - default: `#`
+ * - `Object` style - custom styling options for the card
+ */
+export default function CustomCard(props) {
+  const title = props.title;
+  const description = props.description;
+  const buttonText = props.buttonText;
+  const buttonLink = props.buttonLink || '#';
+  const customStyle = props.style;
+
+  return (
+    <Card style={{ width: 'fit-content', ...customStyle }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <Card.Link href={buttonLink}>{buttonText}</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
