5b8d31afa4decf4e06178e1afd1da2ec4521a50f
Cristian <cristi.valentin29@gmail.com>
2022-02-20 12:33:03 +0100

    feat: 🎸 add http service

---
+++ apps/admin-panel/src/app/utils/http.util.js
@@ -0,0 +1,22 @@
+const httpUtil = {
+    /**
+     * Adds a path parameter to a given path.
+     * @param {String} path - the current path.
+     * @param {String} pathParam - the path parameter to be added to the path.
+     * @returns {String} the path with the added parameter.
+     */
+    addPathParameter: (path, pathParam) => {
+      if (path.endsWith('/')) {
+        path = path.substr(0, path.length - 1);
+      }
+  
+      if (pathParam.startsWith('/')) {
+        pathParam = pathParam.substr(1, pathParam.length);
+      }
+  
+      return `${path}/${pathParam}`;
+    },
+  };
+  
+  export default httpUtil;
+  
