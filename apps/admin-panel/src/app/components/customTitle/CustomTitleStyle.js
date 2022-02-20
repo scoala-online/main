d3cac00e138973f1b24457988a13210e55b98e6b
Cristian <cristi.valentin29@gmail.com>
2022-02-20 11:46:03 +0100

    feat: 🎸 add custom title component

---
+++ apps/admin-panel/src/app/components/customTitle/CustomTitleStyle.js
@@ -0,0 +1,18 @@
+const customTitleStyle = {
+  containerStyle: {
+    marginTop: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  dividerStyle: {
+    borderTop: '3px solid #bbb',
+    borderRadius: '5px',
+    backgroundColor: 'grey',
+    width: '90%',
+  },
+};
+
+export default customTitleStyle;
