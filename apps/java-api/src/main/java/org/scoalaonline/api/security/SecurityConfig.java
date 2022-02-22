318dcc8a764aa0a7b8b6528f9f89e950d397b29c
Robert Mrn <rmrn56@gmail.com>
2022-02-22 11:08:27 +0200

    feat: 🎸 Add /v2/api-docs endpoint

--- apps/java-api/src/main/java/org/scoalaonline/api/security/SecurityConfig.java
+++ apps/java-api/src/main/java/org/scoalaonline/api/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -110,6 +111,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.addFilter(customAuthenticationFilter);
 
     http.addFilterBefore(new CustomAuthorizationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+  }
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security", "/swagger-ui.html", "/webjars/**");
   }
 
   /**
